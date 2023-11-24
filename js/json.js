20102919810bc2f42a1c220244e37571df3ea3a0
leilou <lilou3899@gmail.com>
2023-11-24 14:59:50 +0100

    Premier commit

---
+++ js/json.js
@@ -0,0 +1,38 @@
+const carsLocation = [
+    {
+        "id": 1,
+        "image": "vehicule1.png",
+        "model":"Peugeot 208",
+        "description": "diesel, 5 portes, AutoRadio, Forfait 1000km",
+        "price": 999,
+        "agence": "Paris",
+    },
+    {
+        "id": 2,
+        "image": "vehicule2.png",
+        "model":"Ford Focus",
+        "description": "diesel, 5 portes, AutoRadio, Forfait 1000km",
+        "price": 999,
+        "agence": "Paris",
+
+    },
+    {
+        "id": 3,
+        "image": "vehicule3.png",
+        "model":"Audi A1",
+        "description": "diesel, 5 portes, AutoRadio, Forfait 1000km",
+        "price": "1 100",
+        "agence": "Paris",
+
+    },
+    {
+        "id": 4,
+        "image": "vehicule4.png",
+        "model":"Opel Mokka",
+        "description": "diesel, 5 portes, AutoRadio, Forfait 1000km",
+        "price": "1 150",
+        "agence": "Paris",
+
+    },
+
+]
